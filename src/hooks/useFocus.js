3efb5cc21b43758f8db5ec8f8d8d5eec90962121
Thomas Dashney <tdashney@theworkinggroup.ca>
2019-01-22 15:11:18 -0500

    remove hooks code

--- src/hooks/useFocus.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react'
-
-function useFocus (ref) {
-  useEffect(() => {
-    ref.current.focus()
-  }, [])
-}
-
-export default useFocus
--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import * as serviceWorker from './serviceWorker'
 
-// Import from './classic/App' for the non-hooks version
-import App from './hooks/App'
+import App from './core/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
